e2a02807902d687a62393fb1dbf3042dcfa7f03e
666kurtt <incentivedie@gmail.com>
2022-04-19 17:45:13 +0400

    add calendar

--- TimeTable/Controllers/TimeTableViewController.swift
+++ TimeTable/Controllers/TimeTableViewController.swift
@@ -17,6 +17,15 @@ class TimeTableViewController: UIViewController {
         calendar.translatesAutoresizingMaskIntoConstraints = false
         return calendar
     }()
+    
+    let showHideButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Open calendar", for: .normal)
+        button.setTitleColor(UIColor.systemGray, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +33,73 @@ class TimeTableViewController: UIViewController {
         view.backgroundColor = .white
         title = "TimeTable"
         
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.scope = .week
+        
         setConstraints()
+        swipeAction()
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
         
     }
+    
+    @objc func showHideButtonTapped() {
+        
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Close calendar", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Open calendar", for: .normal)
+        }
+        
+    }
+    
+// MARK: SwipeGestureRecognizer
+    
+    func swipeAction() {
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        calendar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+        
+    }
+    
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        
+        switch gesture.direction {
+        case .up:
+            showHideButtonTapped()
+        case .down:
+            showHideButtonTapped()
+        default:
+            break
+        }
+        
+    }
+    
+    
+}
+
+// MARK: FSCalendarDataSource, FSCalendarDelegate
+
+extension TimeTableViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+
 }
 
 // MARK: SetConstraints
@@ -34,6 +107,7 @@ class TimeTableViewController: UIViewController {
 extension TimeTableViewController {
     
     func setConstraints() {
+        
         view.addSubview(calendar)
         
         calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
@@ -43,6 +117,15 @@ extension TimeTableViewController {
             calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
             calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+        ])
+        
+        view.addSubview(showHideButton)
+        
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20),
+            showHideButton.widthAnchor.constraint(equalToConstant: 100)
         ])
     }
     
